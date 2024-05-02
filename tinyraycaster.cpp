f7a21f8d381ff78b7ec388a0bebfd0284e729288
sneaky <sean.m.kennedy@wsu.edu>
2024-05-02 16:59:47 -0500

    Finished step 4. Basic raycasting.

--- tinyraycaster.cpp
+++ tinyraycaster.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <cstdint>
 #include <cassert>
+#include <cmath>
+#include <cstdint>
 
 using namespace std;
 
@@ -69,6 +70,7 @@ int main() {
 	
 	float player_x = 3.456;
 	float player_y = 2.345;
+	float player_a = 1.523;
 
 	for (size_t j = 0; j < win_h; j++) {
 		for (size_t i = 0; i < win_w; i++) {
@@ -107,6 +109,17 @@ int main() {
 	}
 
 	draw_rect(framebuffer, win_w, win_h, player_x * rect_w, player_y * rect_h, 5, 5, pack_color(255, 255, 255));
+
+	for (float t = 0; t < 20; t += .05) {
+		float cx = player_x + t*cos(player_a);
+		float cy = player_y + t*sin(player_a);
+		if (map[static_cast<int>(cx) + static_cast<int>(cy) * map_w] != ' ') break;
+
+		size_t pix_x = cx * rect_w;
+		size_t pix_y = cy * rect_h;
+		framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
+	}
+
 	drop_ppm_image("./out.ppm", framebuffer, win_w, win_h);
 
 	return 0;

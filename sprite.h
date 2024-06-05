06675a04db9f5d98a5891a0253fe94eba6949adb
sneaky <sean.m.kennedy@wsu.edu>
2024-06-05 12:33:31 -0500

    Started working on adding sprites. Coords/Minimap Rendering are working.

---
+++ sprite.h
@@ -0,0 +1,11 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <cstdlib>
+
+struct Sprite {
+	float x, y;
+	size_t texture_id;
+};
+
+#endif
--- tinyraycaster.cpp
+++ tinyraycaster.cpp
@@ -12,7 +12,26 @@
 #include "player.h"
 #include "framebuffer.h"
 #include "textures.h"
+#include "sprite.h"
 
+int wall_x_texture_coord(const float hitx, const float hity, Texture &texture_walls) {
+	float x = hitx - floor(hitx + 0.5); // x and y contain (signed) fractional parts of hitx and hity,
+	float y = hity - floor(hity + 0.5); // they vary between -0.5 and +0.5, and one of them is supposed to be very close to 0
+	int texture = x * texture_walls.size;
+	
+	if (std::abs(y) > std::abs(x)) { // determine whether we hit a vertical or horizontal wall
+		texture = y * texture_walls.size;
+	}
+	
+	if (texture < 0) { // handle case where x_texture_coord can be negative
+		texture += texture_walls.size;
+	}
+	assert(texture >= 0 && texture < static_cast<int>(texture_walls.size));
+	
+	return texture;
+}
+
+/*
 int wall_x_texture_coord(const float x, const float y, Texture& texture_walls) {
 	float hitx = x - floor(x + 0.05); // hitx and hity contain (signed) fractional parts of x and y,
 	float hity = y - floor(y + 0.05); // they vary between -0.5 and +0.5, and one of them is supposed to be very close to 0
@@ -29,8 +48,15 @@ int wall_x_texture_coord(const float x, const float y, Texture& texture_walls) {
 
 	return texture;
 }
+*/
 
-void render(FrameBuffer& fb, Map& map, Player& player, Texture& texture_walls) {
+void map_show_sprite(Sprite& sprite, FrameBuffer &fb, Map& map) {
+	const size_t rect_w = fb.w / (map.w * 2);
+	const size_t rect_h = fb.h / map.h;
+	fb.draw_rect(sprite.x * rect_w - 3, sprite.y * rect_h - 3, 6, 6, pack_color(255, 0, 0));
+}
+
+void render(FrameBuffer& fb, Map& map, Player& player, std::vector<Sprite> &sprites, Texture& texture_walls, Texture& texture_monster) {
 	fb.clear(pack_color(255, 255, 255)); // clear the screen
 	
 	const size_t rect_w = fb.w / (map.w * 2);
@@ -57,7 +83,9 @@ void render(FrameBuffer& fb, Map& map, Player& player, Texture& texture_walls) {
 
 			size_t texture_id = map.get(x, y);
 			assert(texture_id < texture_walls.count);
-			size_t column_height = fb.h / (t * cos(angle - player.a));
+			//size_t column_height = fb.h / (t * cos(angle - player.a));
+			float dist = t * cos(angle - player.a);
+			size_t column_height = fb.h / dist;
 			int x_texture_coord = wall_x_texture_coord(x, y, texture_walls);
 			std::vector<uint32_t> column = texture_walls.get_scaled_column(texture_id, x_texture_coord, column_height);
 			int pix_x = i + fb.w / 2;
@@ -70,6 +98,10 @@ void render(FrameBuffer& fb, Map& map, Player& player, Texture& texture_walls) {
 			break;
 		}
 	}
+
+	for (size_t i = 0; i < sprites.size(); i++) {
+		map_show_sprite(sprites[i], fb, map);
+	}
 }
 
 int main() {
@@ -77,11 +109,15 @@ int main() {
 	Player player{3.456, 2.345, 1.523, M_PI/3.0};
 	Map map;
 	Texture texture_walls("./walltext.png");
-	if (!texture_walls.count) {
+	Texture texture_monsters("./monsters.png");
+	if (!texture_walls.count || !texture_monsters.count) {
 		std::cerr << "Failed to load wall textures" << std::endl;
 		return -1;
 	}
 
+	std::vector<Sprite> sprites{ {1.834, 8.765, 0}, {5.323, 5.365, 1}, {4.123, 10.265, 1} };
+	
+	/*
 	for (size_t frame = 0; frame < 360; frame++) {
 		std::stringstream ss;
 		ss << std::setfill('0') << std::setw(5) << frame << ".ppm";
@@ -89,6 +125,9 @@ int main() {
 		render(fb, map, player, texture_walls);
 		drop_ppm_image(ss.str(), fb.img, fb.w, fb.h);
 	}
+	*/
 
+	render(fb, map, player, sprites, texture_walls, texture_monsters);
+	drop_ppm_image("./out.ppm", fb.img, fb.w, fb.h);
 	return 0;
 }
